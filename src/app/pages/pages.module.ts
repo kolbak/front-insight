c4aadd42954ac5549df4c8a4b6d96d377d3ce425
Kawaii Uroboros <kawaiiuroboros@gmail.com>
2020-08-12 22:27:36 +0300

    m

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule, NbButtonModule, NbTreeGridModule } from '@nebular/theme';
+import { NbMenuModule, NbButtonModule, NbTreeGridModule,NbProgressBarModule } from '@nebular/theme';
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
@@ -10,7 +10,7 @@ import { NbIconModule, NbCardModule } from '@nebular/theme';
 @NgModule({
   imports: [
     PagesRoutingModule,
-    ThemeModule,
+    ThemeModule,NbProgressBarModule ,
     NbMenuModule,
     NbButtonModule,
     DashboardModule, 
