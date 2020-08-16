aabf3eacf07a162eedaf253d59ac4aca4767e6c4
Попов Виталий <msk.vitaly@gmail.com>
2020-08-16 23:34:39 +0300

    working terminal without commands

--- src/app/pages/users/terminal/terminal.component.ts
+++ src/app/pages/users/terminal/terminal.component.ts
@@ -1,54 +1,36 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-terminal',
   templateUrl: './terminal.component.html',
   styleUrls: ['./terminal.component.scss']
 })
-export class TerminalComponent implements OnInit, AfterContentInit {
+export class TerminalComponent  {
 
+  cmdHistory: string[] = new Array();
+  
   constructor() { }
-
-  ngOnInit(): void {
-  }
   
-  ngAfterContentInit() {
-    this.cmd = document.querySelector('#terminal');
-  }
   command_history: string[];
-  cmd: HTMLDivElement;
   current_path: string = "> ";
-  
-  changePath(path) {
-    this.current_path = path;
-  }
 
   onChange(event: KeyboardEvent) {
     if (event.key != "Enter") return;
     
     // Сохраняем введённое
     let allCommands = document.querySelectorAll('input');
-    let command = allCommands[allCommands.length - 1].value
-    allCommands[allCommands.length - 1].remove();
-    this.cmd.innerHTML += `<span>${command}</span>`
+    let command = allCommands[allCommands.length - 1].value;
+    allCommands[allCommands.length - 1].value = "";
     
+    this.cmdHistory.push(command);
+
+
     // ОТВЕТ
-    let answer = document.createElement('p')
-    answer.innerHTML = `Ответ очка: ${this.current_path}`;
-    this.cmd.appendChild(answer);
-
-
-    // Добавляем возможность писать
-    this.cmd.innerHTML += `
-      <span>${this.current_path}</span><input type="text" (keypress)="onChange($event)" autofocus>
-    `
+    this.cmdHistory.push(`${this.current_path} -- тут ответ`);
   }
   // Сделать здесь список команд и обработчиков. Обрабатывать при нажатии [Enter]
 
   // Можно сделать с помощью этой штуки. Аналог shift
   // https://akveo.github.io/nebular/docs/components/autocomplete/overview#nbautocompletedirective
 
-  // Нужно сделать автофокус на последний input
-
-  // Почему-то не применяются стили...
 }
