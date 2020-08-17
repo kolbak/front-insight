c4b64e9f05ff0fa9795af63a8d4419c31ef98770
Kolbak <kushaevmarat@mail.ru>
2020-08-17 13:05:41 +0300

    corrected charts 6, 4

--- src/app/pages/dashboard/charts/chart4/chart4.component.ts
+++ src/app/pages/dashboard/charts/chart4/chart4.component.ts
@@ -3,15 +3,35 @@ import {
   ChartComponent,
   ApexAxisChartSeries,
   ApexChart,
+  ApexYAxis,
   ApexXAxis,
-  ApexTitleSubtitle
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexMarkers,
+  ApexTooltip,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexDataLabels,
+  ApexLegend,
+  ApexFill
 } from "ng-apexcharts";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
+  yaxis: ApexYAxis;
   xaxis: ApexXAxis;
   title: ApexTitleSubtitle;
+  stroke: ApexStroke;
+  grid: ApexGrid;
+  markers: ApexMarkers;
+  tooltip: ApexTooltip;
+  plotOptions: ApexPlotOptions;
+  responsive: ApexResponsive;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
+  fill: ApexFill
 };
 @Component({
   selector: 'ngx-chart4',
@@ -27,18 +47,95 @@ export class Chart4Component implements AfterContentInit{
     this.chartOptions = {
       series: [{
           name: "My-series",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          data: [50, 100, 0, 60, 20, 30]
         }
       ],
       chart: {
-        height: 345,
-        type: "area"
+        height: 100,
+        offsetY: -25,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
+        },
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          gradientToColors: ["white"],
+          type: "horizontal",
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 30, 70, 100]
+        }
+      },
+      stroke: {
+        show: true,
+        curve: 'smooth',
+        colors: ['#8D77FE'],
+        width: 4
       },
       title: {
-        text: "My First Angular Chart"
+        text: "Chart 4",
+        offsetY: 25,
+      },
+      yaxis: {
+        show: false,
       },
       xaxis: {
-        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+        type: 'category',
+        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun"],
+        labels: {
+            show: false,
+        },
+        axisBorder: {
+            show: false,
+        },
+        crosshairs: {
+            show: false,
+        },
+        tooltip: {
+            enabled: true,
+        }
+      },
+      grid: {
+        xaxis: {
+          lines: {
+            show: false,
+          }
+        },
+        yaxis: {
+          lines: {
+            show: false,
+          }
+        },
+      },
+      markers: {
+        colors: '#8D77FE',
+        strokeColors: 'white',
+        strokeWidth: 3,
+        hover: {
+          size: 7,
+          sizeOffset: 3
+        }
+      },
+      tooltip: {
+        enabled: true,
+        theme: 'light',
+        marker: {
+            show: false,
+        },
+        x: {
+          show: false
+        },
+        fixed: {
+          enabled: true,
+          position: 'topRight',
+          offsetX: 0,
+          offsetY: 0,
+        },
       }
     };
   }
--- src/app/pages/dashboard/charts/chart6/chart6.component.ts
+++ src/app/pages/dashboard/charts/chart6/chart6.component.ts
@@ -3,15 +3,35 @@ import {
   ChartComponent,
   ApexAxisChartSeries,
   ApexChart,
+  ApexYAxis,
   ApexXAxis,
-  ApexTitleSubtitle
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexMarkers,
+  ApexTooltip,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexDataLabels,
+  ApexLegend,
+  ApexFill
 } from "ng-apexcharts";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
+  yaxis: ApexYAxis;
   xaxis: ApexXAxis;
   title: ApexTitleSubtitle;
+  stroke: ApexStroke;
+  grid: ApexGrid;
+  markers: ApexMarkers;
+  tooltip: ApexTooltip;
+  plotOptions: ApexPlotOptions;
+  responsive: ApexResponsive;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
+  fill: ApexFill
 };
 @Component({
   selector: 'ngx-chart6',
@@ -27,18 +47,96 @@ export class Chart6Component implements AfterContentInit{
     this.chartOptions = {
       series: [{
           name: "My-series",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          data: [100, 80, 0, 100, 20, 110]
         }
       ],
       chart: {
-        height: 345,
-        type: "area"
+        offsetY: -15,
+        height: 100,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
+        },
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          gradientToColors: ["white"],
+          type: "horizontal",
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 30, 70, 100]
+        }
+      },
+      stroke: {
+        show: true,
+        curve: 'smooth',
+        colors: ['orange'],
+        width: 4,
+        dashArray: [5]
       },
       title: {
-        text: "My First Angular Chart"
+        text: "Chart 6",
+        offsetY: 15,
+      },
+      yaxis: {
+        show: false,
       },
       xaxis: {
-        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+        type: 'category',
+        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun"],
+        labels: {
+            show: false,
+        },
+        axisBorder: {
+            show: false,
+        },
+        crosshairs: {
+            show: false,
+        },
+        tooltip: {
+            enabled: true,
+        }
+      },
+      grid: {
+        xaxis: {
+          lines: {
+            show: false,
+          }
+        },
+        yaxis: {
+          lines: {
+            show: false,
+          }
+        },
+      },
+      markers: {
+        colors: 'orange',
+        strokeColors: 'white',
+        strokeWidth: 3,
+        hover: {
+          size: 7,
+          sizeOffset: 3
+        }
+      },
+      tooltip: {
+        enabled: true,
+        theme: 'light',
+        marker: {
+            show: false,
+        },
+        x: {
+          show: false
+        },
+        fixed: {
+          enabled: true,
+          position: 'topRight',
+          offsetX: 0,
+          offsetY: 0,
+        },
       }
     };
   }
