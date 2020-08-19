989da1f38293827aec2b1d01e3ebbdaa2bd23828
Попов Виталий <msk.vitaly@gmail.com>
2020-08-19 23:42:58 +0300

    files page; common info

--- src/app/pages/files/all-files/all-files.component.ts
+++ src/app/pages/files/all-files/all-files.component.ts
@@ -1,6 +1,14 @@
 
 import { Component } from '@angular/core';
 
+export interface folderInfo {
+  folderName: string;
+  extensions: string[];
+  amount: string;
+  totalVolume: string; 
+}
+
+
 @Component({
   selector: 'ngx-all-files',
   templateUrl: './all-files.component.html',
@@ -8,7 +16,35 @@ import { Component } from '@angular/core';
 })
 export class AllFilesComponent {  
 
-  constructor() { }
+  files: folderInfo[] = [];
+  extensions: string[][] = [
+    ["zip", "rar"], 
+    ["doc"], 
+    ["xlsx"],
+    ["pdf"],
+    ["png", "jpeg", "bmp", "gif"],
+    ["mp3", "wav"] ,
+    ["zip", "rar"], 
+    ["doc"], 
+    ["xlsx"],
+    ["pdf"],
+    ["png", "jpeg", "bmp", "gif"],
+    ["mp3", "wav"] 
+  ];
+  folderNames: string[] = ["Архивы", "Word файлы", "Excel файлы", "Pdf файлы", "Фото", "Музыка",
+  "Архивы", "Word файлы", "Excel файлы", "Pdf файлы", "Фото", "Музыка",
+]
 
-  files: string[] = ["png", "jpeg", "bmp", "doc", "json", "exe", "gif", "xlsx"];
+  constructor() { 
+    for (let i = 0; i < this.folderNames.length; i++) {
+      this.files.push({
+        folderName: this.folderNames[i],
+        extensions: this.extensions[i],
+        amount: `${Math.floor(Math.random()*1000)}`, 
+        totalVolume:`${Math.floor(Math.random()*1000)}`
+      })
+      
+    }
+  }
+
 }
