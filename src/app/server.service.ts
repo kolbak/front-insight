ab4cdb7a6e96cea3965a1ffa2e7caa1beaa641fa
Kawaii Uroboros <kawaiiuroboros@gmail.com>
2020-08-22 00:54:25 +0300

    new class

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,16 +1,29 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+
+export class User
+{
+  "uuid":string;
+  "company":string;
+  "sector":string;
+  "first_name":string;
+  "middle_name":string;
+  "last_name":string;
+  "full_name":string;
+  "photo":string;
+  "role":string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
-  allusers:any = ""
+  allusers: [User]
   public HOST = "http://localhost:4200/api";
   constructor(private http : HttpClient) { }
   getAllUsers(){
-    this.http.get(this.HOST+"/users").subscribe((response)=>{
+    this.http.get(this.HOST+"/users").subscribe((response : [User])=>{
 this.allusers = response
     })
   }
