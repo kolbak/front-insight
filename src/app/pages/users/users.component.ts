7fbc08c086c070c72961a9cffc8e7d2fc448a6c9
Попов Виталий <msk.vitaly@gmail.com>
2020-08-26 04:40:43 +0300

    users menu

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -25,21 +25,50 @@ export class UsersComponent implements OnInit {
     { name: 'Janitor', title: 'Janitor',picture:'https://ih1.redbubble.net/image.846505055.7003/flat,750x1000,075,f.u3.jpg' },
     { name: 'Perry Cox', title: 'Doctor of Medicine' ,picture:'https://i.imgur.com/2epwyls.jpg'},
   ];
+
+  menu: {title: string, url: string, routerLink: string, active: boolean}[] = [
+    {title: 'Загрузки',  url: '../../../assets/images/user-actions/31996.svg', routerLink: '/pages/users/downloads',   active: false},
+    {title: 'Скриншоты', url: '../../../assets/images/user-actions/31997.svg', routerLink: '/pages/users/screenshots', active: false},
+    {title: 'Видео',     url: '../../../assets/images/user-actions/32001.svg', routerLink: '/pages/users/videos',      active: false},
+    {title: 'Кейлоггер', url: '../../../assets/images/user-actions/31995.svg', routerLink: '/pages/users/keylog',      active: false},
+    {title: 'Документы', url: '../../../assets/images/user-actions/31998.svg', routerLink: '/pages/users/documents',   active: false},
+    {title: 'Proxy',     url: '../../../assets/images/user-actions/31999.svg', routerLink: '/pages/users/proxy',       active: false},
+    {title: 'Терминал',  url: '../../../assets/images/user-actions/32000.svg', routerLink: '/pages/users/terminal',    active: false},
+  ];
+
+  firstTime: boolean = true;
   public MAINUSER:string;
   constructor(public server: ServerService,public router: Router) {
      server.getAllUsers();
 
-     //this.MAINUSER = server.allusers..uuid;
-
+    //  this.MAINUSER = server.allusers[0].uuid;
    }
   ngOnInit(){
+    // this.MAINUSER = this.server.allusers[0].uuid;
+    // console.log('this.MAINUSER :>> ', this.MAINUSER);
   }
 
-
   GetUserToRoute(uuid:string){
+    if (this.firstTime && uuid == this.server.allusers[0].uuid) {
+      return;
+    }
+    this.firstTime = false;
     this.MAINUSER = uuid;
     this.server.editUuid(uuid);
     this.router.navigate(['/pages/users/default']);
   }
 
+  sectionClick(section)
+  {
+    if (section.active) {
+      this.server.editUuid(this.MAINUSER);
+      this.router.navigate(['/pages/users/default']);
+      section.active = false;
+      return;
+    }
+    this.menu.forEach(element => {
+      element.active = false;
+    });
+    section.active = true;
+  }
 }
