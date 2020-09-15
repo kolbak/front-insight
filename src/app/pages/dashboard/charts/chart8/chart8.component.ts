12fc73ef6d2cb2ba5f927389dd80bc9a08ea007c
Kolbak <kushaevmarat@mail.ru>
2020-09-16 01:23:59 +0300

    charts ngx

--- src/app/pages/dashboard/charts/chart8/chart8.component.ts
+++ src/app/pages/dashboard/charts/chart8/chart8.component.ts
@@ -1,97 +1,146 @@
-import { Component, ViewChild } from "@angular/core";
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexYAxis,
-  ApexXAxis,
-  ApexTitleSubtitle,
-  ApexStroke,
-  ApexGrid,
-  ApexMarkers,
-  ApexTooltip,
-  ApexPlotOptions,
-  ApexResponsive,
-  ApexDataLabels,
-  ApexLegend,
-  ApexFill
-} from "ng-apexcharts";
+import { Component } from '@angular/core';
 
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  yaxis: ApexYAxis;
-  xaxis: ApexXAxis;
-  title: ApexTitleSubtitle;
-  stroke: ApexStroke;
-  grid: ApexGrid;
-  markers: ApexMarkers;
-  tooltip: ApexTooltip;
-  plotOptions: ApexPlotOptions;
-  responsive: ApexResponsive;
-  dataLabels: ApexDataLabels;
-  legend: ApexLegend;
-  fill: ApexFill
-};
 @Component({
   selector: 'ngx-chart8',
   templateUrl: './chart8.component.html',
   styleUrls: ['./chart8.component.scss']
 })
-export class Chart8Component{
-  
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: Partial<any>;
-  
-  constructor() {
-    this.chartOptions = {
-      series: [80, 30, 60],
-      labels: ['Social', 'Email', 'Search'],
-      chart: {
-        type: 'donut',
-        height: 230
-      },
-      title: {
-        text: 'Chart 8'
-      },
-      fill: {
-        colors: ['#5A8DEE', '#00CFDD', '#FDAC41']
-      },
-      stroke: {
-        show: false
-      },
-      plotOptions: {
-        pie: {
-          expandOnClick: false,
-          donut: {
-            size: '87%',
-            labels: {
-              show: true,
-              name: {
-                show: true,
-              },
-              value: {
-                show: true,
-              },
-              total: {
-                show: true,
-                color: 'black'
-              }
-            }
-          },
-        }
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: true,
-        position: 'bottom',
-        offsetY: -10,
-        markers: {
-          fillColors: ['#5A8DEE', '#00CFDD', '#FDAC41']
-        }
-      }
+export class Chart8Component {
+  // options
+  gradient: boolean = true;
+  isDoughnut: boolean = true;
+  // showLegend: boolean = true;
+  // legendPosition: string = 'right';
+  colorScheme = {
+    domain: ['#5A8DEE', '#00CFDD', '#FDAC41']
+  };
+
+  single = this.genSingleData();
+  genSingleData() {
+    let labels = ["Social", "Email", "Search"];
+    let arr = [];
+    for(let i = 0, l = 3; i < l; i++){
+      let obj = {
+        name : '',
+        value: 0
+      };
+      obj.name  = labels[i];
+      obj.value = Math.floor(1 + Math.random() * 99);
+      arr.push(obj);
     };
+    return arr;
   }
+
+  constructor() {
+    Object.assign(this, this.single);
+  }
+
+  onSelect(data): void { }
+  onActivate(data): void { }
+  onDeactivate(data): void { }
 }
+
+
+
+
+
+
+
+
+// import { Component, ViewChild } from "@angular/core";
+// import {
+//   ChartComponent,
+//   ApexAxisChartSeries,
+//   ApexChart,
+//   ApexYAxis,
+//   ApexXAxis,
+//   ApexTitleSubtitle,
+//   ApexStroke,
+//   ApexGrid,
+//   ApexMarkers,
+//   ApexTooltip,
+//   ApexPlotOptions,
+//   ApexResponsive,
+//   ApexDataLabels,
+//   ApexLegend,
+//   ApexFill
+// } from "ng-apexcharts";
+
+// export type ChartOptions = {
+//   series: ApexAxisChartSeries;
+//   chart: ApexChart;
+//   yaxis: ApexYAxis;
+//   xaxis: ApexXAxis;
+//   title: ApexTitleSubtitle;
+//   stroke: ApexStroke;
+//   grid: ApexGrid;
+//   markers: ApexMarkers;
+//   tooltip: ApexTooltip;
+//   plotOptions: ApexPlotOptions;
+//   responsive: ApexResponsive;
+//   dataLabels: ApexDataLabels;
+//   legend: ApexLegend;
+//   fill: ApexFill
+// };
+// @Component({
+//   selector: 'ngx-chart8',
+//   templateUrl: './chart8.component.html',
+//   styleUrls: ['./chart8.component.scss']
+// })
+// export class Chart8Component{
+  
+//   @ViewChild("chart") chart: ChartComponent;
+//   public chartOptions: Partial<any>;
+  
+//   constructor() {
+//     this.chartOptions = {
+//       series: [80, 30, 60],
+//       labels: ['Social', 'Email', 'Search'],
+//       chart: {
+//         type: 'donut',
+//         height: 230
+//       },
+//       title: {
+//         text: 'Chart 8'
+//       },
+//       fill: {
+//         colors: ['#5A8DEE', '#00CFDD', '#FDAC41']
+//       },
+//       stroke: {
+//         show: false
+//       },
+//       plotOptions: {
+//         pie: {
+//           expandOnClick: false,
+//           donut: {
+//             size: '87%',
+//             labels: {
+//               show: true,
+//               name: {
+//                 show: true,
+//               },
+//               value: {
+//                 show: true,
+//               },
+//               total: {
+//                 show: true,
+//                 color: 'black'
+//               }
+//             }
+//           },
+//         }
+//       },
+//       dataLabels: {
+//         enabled: false,
+//       },
+//       legend: {
+//         show: true,
+//         position: 'bottom',
+//         offsetY: -10,
+//         markers: {
+//           fillColors: ['#5A8DEE', '#00CFDD', '#FDAC41']
+//         }
+//       }
+//     };
+//   }
+// }
